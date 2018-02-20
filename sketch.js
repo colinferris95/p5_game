0484e9200ff5217f21005235f70e7e4ffb66af94
Colin Ferris <colinferris95@gmail.com>
2018-02-20 17:21:27 +0000

    oh look a green square

--- sketch.js
+++ sketch.js
@@ -1,7 +1,14 @@
 function setup() {
-
+	createCanvas(640, 480);
+	
 }
 
 function draw() {
-  ellipse(50, 50, 80, 80);
+	background(51);
+  if (mouseIsPressed) {
+    fill(0,100,0);
+  } else {
+    fill(50,205,50);
+  }
+  rect(mouseX, 350, 80, 80);
 }
