2bc5c232b62dd1fefac5c15e8f1819f95ff4065c
Colin Ferris <colinferris95@gmail.com>
2018-02-20 19:23:39 +0000

    oh look a ball

--- sketch.js
+++ sketch.js
@@ -1,3 +1,7 @@
+//globals
+var block_x_position = 320;
+var block_y_position = 0;
+
 function setup() {
 	createCanvas(640, 480);
 	
@@ -5,10 +9,14 @@ function setup() {
 
 function draw() {
 	background(51);
-  if (mouseIsPressed) {
-    fill(0,100,0);
-  } else {
-    fill(50,205,50);
-  }
-  rect(mouseX, 350, 80, 80);
+	
+	block_y_position = block_y_position + 5;
+	fill(0,100,0);
+	ellipse(block_x_position, block_y_position, 80, 80);
+	if (mouseIsPressed) {
+		fill(0,100,0);
+	} else {
+		fill(50,205,50);
+	}
+	rect(mouseX, 350, 80, 80);
 }
