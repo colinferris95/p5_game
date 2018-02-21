b1ccbca36284a762fa75d71eac23f2aa7a643271
Colin Ferris <colinferris95@gmail.com>
2018-02-21 21:05:22 +0000

    added p5.dom library and a reset button

--- sketch.js
+++ sketch.js
@@ -5,10 +5,23 @@ var block_x_position = 320;
 var block_y_position = 0;
 var score = 0;
 var game_loop = true;
-
+var input, button, greeting;
 function setup() {
 	createCanvas(640, 480);
 	
+	
+
+  button = createButton('reset');
+  button.position(20, 65);
+  button.mousePressed(reset);
+
+ 
+}
+
+function reset() {
+	
+	game_loop = true;
+	score = 0;
 }
 //add to score every second
 window.setInterval(
@@ -24,10 +37,11 @@ function print_score(){
 
 function ball_object(){
 	//make the ball move downward
-	block_y_position = block_y_position + 5;
+	block_y_position = block_y_position + 8;
 	if (block_y_position > 500){
 		//reset ball
 		block_y_position = 0;
+		block_x_position = Math.floor(Math.random() * 640);
 		
 	}
 	fill(50,205,50);
@@ -40,6 +54,7 @@ function hit_detection(){
 	if (mouseX < block_x_position && mouseX > block_x_position - 100 && 350 < block_y_position && 350 > block_y_position - 100){
 		fill(128,128,0);
 		game_loop = false;
+		//end game
 	}
 	
 }
@@ -51,9 +66,9 @@ function player(){
 }
 
 function draw() {
-	
+	//start game loop
 	if (game_loop){
-	
+		//game processes 
 		background(51);
 		print_score();
 		ball_object();
@@ -62,7 +77,7 @@ function draw() {
 		
 	}
 	else{
-	
+		//fail screen
 		background(255);
 	
 	}
